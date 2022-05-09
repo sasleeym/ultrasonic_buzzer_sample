30d502d2b93c25454fa183e2d6dc6f8ed74eacb0
sasleeym <104290503+sasleeym@users.noreply.github.com>
2022-05-09 18:15:11 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
+music.setVolume(127)
 basic.forever(function () {
+    serial.writeLine("" + (sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )))
     basic.showNumber(sonar.ping(
     DigitalPin.P1,
     DigitalPin.P2,
@@ -8,10 +14,7 @@ basic.forever(function () {
     DigitalPin.P1,
     DigitalPin.P2,
     PingUnit.Centimeters
-    ) < 5) {
+    ) > 5) {
         music.playTone(988, music.beat(BeatFraction.Whole))
     }
-})
-basic.forever(function () {
-	
 })

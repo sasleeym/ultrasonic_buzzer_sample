8073febe109a0759d5aeb8840a407741ff196692
sasleeym <104290503+sasleeym@users.noreply.github.com>
2022-06-13 10:06:41 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,21 @@
-music.setVolume(255)
 basic.forever(function () {
     serial.writeLine("" + (sonar.ping(
     DigitalPin.P1,
     DigitalPin.P2,
     PingUnit.Centimeters
     )))
-    basic.showNumber(sonar.ping(
+    if (sonar.ping(
     DigitalPin.P1,
     DigitalPin.P2,
     PingUnit.Centimeters
-    ))
+    ) > 0) {
+    	
+    } else {
+    	
+    }
 })
 basic.forever(function () {
+    music.setVolume(255)
     music.playTone(880, music.beat(BeatFraction.Half))
     basic.pause(200)
     music.playTone(988, music.beat(BeatFraction.Half))
